080a79c27ba73137d84a59b6d9cbf5fd966baf76
weiyaocsee <weiyaocsee@gmail.com>
2021-02-23 13:11:53 +0800

    first

---
+++ src/ch02/first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int num;
+    num = 1;
+
+    printf("I am a simple ");
+    printf("compter.\n");
+    printf("My favorite number is %d because it is the first.\n", num);
+
+    return 0;
+}
